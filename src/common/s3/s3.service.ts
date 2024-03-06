7218ac91c0130ffa7966abdc0dff6c0554735c74
Louis <andrewng23@proton.me>
2024-03-05 17:29:15 -0700

    use credentials from environment only if they are provided

--- src/common/s3/s3.service.ts
+++ src/common/s3/s3.service.ts
@@ -1,6 +1,6 @@
+import { PutObjectCommand, S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PutObjectCommand, S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
 import { FileUploadOptions } from './file-upload-options.interface';
 
 @Injectable()
@@ -8,15 +8,13 @@ export class S3Service {
   private readonly client: S3Client;
 
   constructor(configService: ConfigService) {
-    const accessKeyId = configService.get<string>('AWS_ACCESS_KEY');
-    const secretAccessKey = configService.get<string>('AWS_SECRET_ACCESS_KEY');
-    const region = configService.get<string>('AWS_REGION', 'us-east-2'); // Default to 'us-east-1' if not specified
+    const accessKeyId = configService.get('AWS_ACCESS_KEY');
+    const secretAccessKey = configService.get('AWS_SECRET_ACCESS_KEY');
 
     const clientConfig: S3ClientConfig = {
-      region, // Specify the region in the client configuration
+      region: 'us-east-2',
     };
 
-    // Use credentials from environment variables only if they are provided
     if (accessKeyId && secretAccessKey) {
       clientConfig.credentials = {
         accessKeyId,
