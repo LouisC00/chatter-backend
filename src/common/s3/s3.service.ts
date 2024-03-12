3c575a1f815eca58fa24398f9ded316e0c7bd203
Louis <andrewng23@proton.me>
2024-03-12 15:58:25 -0600

    Use IAM role for S3 access in Elastic Beanstalk environment

--- src/common/s3/s3.service.ts
+++ src/common/s3/s3.service.ts
@@ -1,28 +1,15 @@
-import { PutObjectCommand, S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { FileUploadOptions } from './file-upload-options.interface';
 
 @Injectable()
 export class S3Service {
   private readonly client: S3Client;
 
-  constructor(configService: ConfigService) {
-    const accessKeyId = configService.get('AWS_ACCESS_KEY');
-    const secretAccessKey = configService.get('AWS_SECRET_ACCESS_KEY');
-
-    const clientConfig: S3ClientConfig = {
+  constructor() {
+    this.client = new S3Client({
       region: 'us-east-2',
-    };
-
-    if (accessKeyId && secretAccessKey) {
-      clientConfig.credentials = {
-        accessKeyId,
-        secretAccessKey,
-      };
-    }
-
-    this.client = new S3Client(clientConfig);
+    });
   }
 
   async upload({ bucket, key, file }: FileUploadOptions) {
